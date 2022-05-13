85b583baab7b3e81d1c3f974a01df096ed803189
HuongLam <huong.lam@devsamurai.com>
2022-05-13 17:32:40 +0700

    update on iOS

--- ios/SolareumSdk.m
+++ ios/SolareumSdk.m
@@ -2,6 +2,6 @@
 
 @interface RCT_EXTERN_MODULE(SolareumSdk, NSObject)
 
-RCT_EXTERN_METHOD(open);
+RCT_EXTERN_METHOD(open: (NSString *)payment );
 
 @end
--- ios/SolareumSdk.swift
+++ ios/SolareumSdk.swift
@@ -19,38 +19,58 @@ class SolareumSdk: NSObject {
     
  
  @objc
-  func open() -> Void{
-      let urlScheme = externalURLScheme();
-    if  let solareumUrl = URL(string:"solareum://app?address=D4aw7jPLJLEXqmKf9nQfMCbevtuVX4b1PkuKx5p2H1jx&token=XSB&scheme=\(urlScheme!)"){
-      DispatchQueue.main.async {
-          if #available(iOS 10.0, *) {
-              UIApplication.shared.open(solareumUrl, options: [:], completionHandler: {
-                  (success) in
-                  if (success)
-                  {
-                      print("OPENED \(solareumUrl): \(success)")
-                  }
-                  else
-                  {
-                      let universalLink = URL(string: "https://solareum.page.link/rewards?address=D4aw7jPLJLEXqmKf9nQfMCbevtuVX4b1PkuKx5p2H1jx&token=XSB")
-                      UIApplication.shared.open(universalLink!, options: [:], completionHandler: {
-                          (success) in
-                          if (success)
-                          {
-                              print("OPENED \(String(describing: universalLink)): \(success)")
-                          }
-                          else
-                          {
-                              
-                          }
-                      })
-                  }
-              })
-          } else {
-              // Fallback on earlier versions
-          }
-      }
-    }
+    func open(_ payment: NSString) -> Void{
+        var dictionary:NSDictionary?
+            
+        if let data = payment.data(using: String.Encoding.utf8.rawValue) {
+                
+             do {
+               dictionary =  try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String:AnyObject] as NSDictionary?
+                
+                    if let paymentDictionary = dictionary
+                      {
+                        let address = paymentDictionary["address"]
+                        let token = paymentDictionary["token"]
+                        let scheme = paymentDictionary["scheme"]
+                        let client_id = paymentDictionary["client_id"]
+
+                      let urlScheme = externalURLScheme()
+                      if  let solareumUrl = URL(string:"solareum://app?address=\(address!)&token=\(token!)&client_id=\(client_id!)&scheme=\(urlScheme!)"){
+                        DispatchQueue.main.async {
+                            if #available(iOS 10.0, *) {
+                                UIApplication.shared.open(solareumUrl, options: [:], completionHandler: {
+                                    (success) in
+                                    if (success)
+                                    {
+                                        print("OPENED \(solareumUrl): \(success)")
+                                    }
+                                    else
+                                    {
+                                        let universalLink = URL(string: "https://solareum.page.link/rewards?address=\(address!)&token=\(token!)")
+                                        UIApplication.shared.open(universalLink!, options: [:], completionHandler: {
+                                            (success) in
+                                            if (success)
+                                            {
+                                                print("OPENED \(String(describing: universalLink)): \(success)")
+                                            }
+                                            else
+                                            {
+                                                
+                                            }
+                                        })
+                                    }
+                                })
+                            } else {
+                                // Fallback on earlier versions
+                            }
+                        }
+                      }
+                      }
+                    } catch let error as NSError {
+                    
+                    print(error)
+                 }
+        }
   }
     
 func externalURLScheme() -> String? {
